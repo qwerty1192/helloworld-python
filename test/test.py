ef703a45ef297e2109bf45b54bb5e78acc24cc24
qwerty1192 <qwerty1192@users.noreply.github.com>
2022-07-23 13:23:23 +0900

    🧤

---
+++ test/test.py
@@ -0,0 +1,25 @@
+print(111+189)
+
+a = 789
+b = 34.5
+c = a * b
+d = "Hello world!"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(a*b)
+
+print(type(a))
+print(type(b))
+print(type(d))
+
+a , b = b , a
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
