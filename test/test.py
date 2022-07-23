11f95c3dc347ebb66245814aaf828cfa86b59ebb
qwerty1192 <qwerty1192@users.noreply.github.com>
2022-07-23 15:56:43 +0900

    🌳

--- test/test.py
+++ test/test.py
@@ -11,15 +11,22 @@ print(c)
 print(d)
 
 print(a*b)
+print(a-c)
+print(a/b)
+print(a+b)
 
 print(type(a))
 print(type(b))
 print(type(d))
 
 a , b = b , a
-
 print(a)
 print(b)
-
 print(type(a))
 print(type(b))
+
+e = "100"
+f = int(e) + b
+print(e)
+print(f)
+print(a+int(e))
